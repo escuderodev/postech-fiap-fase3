5e190469c9891f2d7a9ab2d0f7ae7d614fc3e88d
Fábio Araújo <fabioaraujo289@gmail.com>
2024-10-04 20:17:29 -0300

    fix routes

--- frontend/Post-App/src/router.jsx
+++ frontend/Post-App/src/router.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./Components/Header"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Header";
 import ListPostagens from "./Components/ListPostagens";
 import Posts from "./Components/Posts";
 
-
-
-export default  function RouterApp(){
-    return( 
+export default function RouterApp() {
+    return (
         <BrowserRouter>
-       
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/ListPostagens" element={<ListPostagens/>}/>
-            <Route path="/Posts" element={<Posts/>}/>                
-        </Routes>                  
-        
+            <Routes>
+                <Route path="/login" element={<Home />} />
+                <Route path="/ListPostagens" element={<ListPostagens />} />
+                <Route path="/" element={<Posts />} />
+            </Routes>
         </BrowserRouter>
-    )
+    );
 }
